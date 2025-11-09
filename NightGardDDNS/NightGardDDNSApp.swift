ae9e3693812c2e7e3bf64dca02c19959e672a437
Michael Fluharty <michael.fluharty@mac.com>
2025-11-09 09:24:59 -0600

    Initial Commit

---
+++ NightGardDDNS/NightGardDDNSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightGardDDNSApp.swift
+//  NightGardDDNS
+//
+//  Created by Michael Fluharty on 11/9/25.
+//
+
+import SwiftUI
+
+@main
+struct NightGardDDNSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NightGardDDNSTests/NightGardDDNSTests.swift
@@ -0,0 +1,16 @@
+//
+//  NightGardDDNSTests.swift
+//  NightGardDDNSTests
+//
+//  Created by Michael Fluharty on 11/9/25.
+//
+
+import Testing
+
+struct NightGardDDNSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
